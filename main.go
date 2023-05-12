4b33acc25054178b2b4d268f45b96bfa565e0da0
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-05-12 10:49:14 -0500

    getCurrencyData added with goroutine

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"frontendmasters.com/go/crypto/api"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	go getCurrencyData("BTC")
+	go getCurrencyData("ETH")
+	go getCurrencyData("BCH")
+	time.Sleep(time.Second * 5)
+}
 
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("The rate of %v is %.2f\n", rate.Currency, rate.Price)
 	}
-	print(rate.Price, err)
 }
